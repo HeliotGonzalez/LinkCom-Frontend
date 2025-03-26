8fc39d58f31ca90396faf32fc75f0ccdaf27a846
Luis Muñoz <lumusa2design@gmail.com>
2025-03-26 16:43:34 +0000

    fixed problems  with components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { EventsCommunityCalendarComponent } from './events-community-calendar/events-community-calendar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component:EventsCommunityCalendarComponent}
+];
---
+++ src/app/events-community-calendar/events-community-calendar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events-community-calendar',
+  imports: [],
+  templateUrl: './events-community-calendar.component.html',
+  styleUrl: './events-community-calendar.component.css'
+
+})
+export class EventsCommunityCalendarComponent {
+  protected img_evento: string | null = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7LZrNYPtb4UCleAD_Tf2nxE64eSUfKnRxEg&s"
+}
