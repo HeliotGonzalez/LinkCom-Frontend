e545a8a094c5733fd9b1c472d3596888bd4e8f25
Luis Muñoz <lumusa2design@gmail.com>
2025-04-01 13:54:09 +0100

    added comunities view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { EventsCommunityCalendarComponent } from './events-community-calendar/events-community-calendar.component';
+import { ComunitiesComponent } from './comunities/comunities.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 export const routes: Routes = [
-    {path:"", component:EventsCommunityCalendarComponent}
+    {path:"", component:ComunitiesComponent}
 ];
--- src/app/comunities/comunities.component.ts
+++ src/app/comunities/comunities.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-comunities',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './comunities.component.html',
   styleUrl: './comunities.component.css'
 })
 export class ComunitiesComponent {
-
+  query = '';
+  comunidades = [1, 2, 3, 4]; 
 }
+
+
+
+
+
+
