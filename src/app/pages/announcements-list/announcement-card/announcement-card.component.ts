cb7c61781022dc34f6022f970ee5a43977b563bc
AVR21 <alejandroviera12@gmail.com>
2025-04-29 16:16:11 +0100

    feat: added announcement delete method to serviceFactory.

--- src/app/pages/announcements-list/announcement-card/announcement-card.component.ts
+++ src/app/pages/announcements-list/announcement-card/announcement-card.component.ts
@@ -5,6 +5,8 @@ import { AuthService } from '../../services/auth.service'; // Asegúrate de tene
 import { Announce } from '../../interfaces/announce';
 import Swal from 'sweetalert2';
 import { NgIf } from '@angular/common';
+import { ServiceFactory } from '../../services/api-services/ServiceFactory.service';
+import { CommunityService } from '../../../architecture/services/CommunityService';
 
 @Component({
   selector: 'app-announcement-card',
@@ -21,7 +23,8 @@ export class AnnouncementCardComponent implements OnInit {
   constructor(
     private router: Router, 
     private authService: AuthService, 
-    private apiService: ApiService
+    private apiService: ApiService,
+    private serviceFactory: ServiceFactory
   ) {}
 
   ngOnInit() {
@@ -44,7 +47,7 @@ export class AnnouncementCardComponent implements OnInit {
       if (result.isConfirmed) {
         console.log('ID del anuncio a eliminar:', announcementId);
         // Hacer la llamada al API de eliminación
-        this.apiService.removeAnnouncement(announcementId, this.userId).subscribe({
+        (this.serviceFactory.get('communities') as CommunityService).removeAnnouncement(announcementId).subscribe({
           next: (response) => {
             console.log('Evento eliminado con éxito:', response);
             Swal.fire({
