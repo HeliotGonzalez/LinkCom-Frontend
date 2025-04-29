c2a0e8e687721ce84da932349b44a4f47be357cb
alexxarmaas <alejandro.armas103@alu.ulpgc.es>
2025-04-29 15:51:34 +0100

    feat: added comments modal and button to delete comments

--- src/app/event-view/event-comment-modal/event-comment-modal.component.ts
+++ src/app/event-view/event-comment-modal/event-comment-modal.component.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { NgFor, NgIf } from '@angular/common';
 import {Component, Input, Output, EventEmitter, AfterViewInit, ElementRef, ViewChild, OnChanges, SimpleChanges} from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -11,10 +12,22 @@ import { WebSocketFactory } from '../../services/api-services/WebSocketFactory.s
 declare var bootstrap: any;
 
 @Component({
+=======
+import {
+    Component, Input, Output, EventEmitter,
+    AfterViewInit, ElementRef, ViewChild, OnChanges, SimpleChanges
+  } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+  
+  declare var bootstrap: any;
+  
+  @Component({
+>>>>>>> 6a3686a (feat: added comments modal and button to delete comments)
     selector: 'app-event-comment-modal',
     standalone: true,
     templateUrl: './event-comment-modal.component.html',
     styleUrl: './event-comment-modal.component.css',
+<<<<<<< HEAD
     imports: [FormsModule, NgIf, NgFor]
 })
 export class EventCommentModalComponent implements AfterViewInit, OnChanges {
@@ -123,3 +136,46 @@ export class EventCommentModalComponent implements AfterViewInit, OnChanges {
     }
     
 }
+=======
+    imports: [FormsModule]
+  })
+  export class EventCommentModalComponent implements AfterViewInit, OnChanges {
+    @Input() visible = false;
+    @Output() closed = new EventEmitter<void>();
+    @Output() commentSubmitted = new EventEmitter<string>();
+    @Input() comments: string[] = [];
+    @ViewChild('modal') modalRef!: ElementRef;
+    commentText: string = '';
+  
+    private modalInstance: any;
+  
+    ngAfterViewInit() {
+      this.modalInstance = new bootstrap.Modal(this.modalRef.nativeElement);
+      this.modalRef.nativeElement.addEventListener('hidden.bs.modal', () => {
+        this.closed.emit();
+        this.commentText = ''; // Limpia el campo al cerrar
+      });
+  
+      if (this.visible) this.modalInstance.show();
+    }
+  
+    ngOnChanges(changes: SimpleChanges) {
+      if (!this.modalInstance) return;
+      if (changes['visible']) {
+        this.visible ? this.modalInstance.show() : this.modalInstance.hide();
+      }
+    }
+  
+    closeModal() {
+      this.modalInstance.hide();
+    }
+  
+    submitComment() {
+      if (this.commentText.trim()) {
+        this.commentSubmitted.emit(this.commentText.trim());
+        this.closeModal();
+      }
+    }
+  }
+  
+>>>>>>> 6a3686a (feat: added comments modal and button to delete comments)
