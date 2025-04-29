bd457403470e78e1164e1412ad9198c9f949d6d6
alexxarmaas <alejandro.armas103@alu.ulpgc.es>
2025-04-29 22:28:50 +0100

    Fixed somethigns

--- src/app/services/api-services/HTTPEventService.ts
+++ src/app/services/api-services/HTTPEventService.ts
@@ -4,9 +4,19 @@ import {EventService} from "../../../architecture/services/EventService";
 import {ApiResponse} from "../../interfaces/ApiResponse";
 import {HttpClient} from "@angular/common/http";
 import {User} from "../../../architecture/model/User";
+import { Comment } from "../../../architecture/model/Comment";
+import {EventState} from "../../../architecture/model/EventState";
 
 export class HTTPEventService implements EventService {
     constructor(private http: HttpClient, private url: string) {
+    }
+
+    acceptEvent(eventID: string): Observable<ApiResponse<CommunityEvent>> {
+        return this.http.patch<ApiResponse<CommunityEvent>>(`${this.url}/events/${eventID}/update`, {eventState: EventState.PUBLISHED});
+    }
+
+    getUserCommunityEvents(communityID: string, userID: string): Observable<ApiResponse<CommunityEvent>> {
+        return this.http.get<ApiResponse<CommunityEvent>>(`${this.url}/events/joined/${userID}?communityID=${communityID}`);
     }
 
     getEvents(): Observable<ApiResponse<CommunityEvent>> {
@@ -29,8 +39,8 @@ export class HTTPEventService implements EventService {
         return this.http.delete<ApiResponse<CommunityEvent>>(`${this.url}/events/${eventID}/delete`);
     }
 
-    joinEvent(eventID: string, userID: string): Observable<ApiResponse<CommunityEvent>> {
-        return this.http.put<ApiResponse<CommunityEvent>>(`${this.url}/events/${eventID}/join`, {userID});
+    joinEvent(communityID: string, eventID: string, userID: string): Observable<ApiResponse<CommunityEvent>> {
+        return this.http.put<ApiResponse<CommunityEvent>>(`${this.url}/events/${eventID}/join`, {communityID, userID});
     }
 
     leaveEvent(eventID: string, userID: string): Observable<ApiResponse<CommunityEvent>> {
@@ -40,4 +50,14 @@ export class HTTPEventService implements EventService {
     getMembers(eventID: string): Observable<ApiResponse<User>> {
         return this.http.get<ApiResponse<User>>(`${this.url}/events/${eventID}/members`);
     }
+
+    createComment(comment: Comment): Observable<ApiResponse<Comment>> {
+        return this.http.post<ApiResponse<Comment>>(`${this.url}/events/${comment.eventID}/createComment`, comment);
+    }
+
+    getComments(eventID: string): Observable<ApiResponse<Comment>> {
+        return this.http.get<ApiResponse<Comment>>(`${this.url}/events/${eventID}/getComments`);
+    }
+
+
 }
