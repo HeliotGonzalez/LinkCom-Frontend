d5f3d9fdf83441e4a939ae95e635d1371c126ca6
alexxarmaas <alejandro.armas103@alu.ulpgc.es>
2025-05-01 18:28:15 +0100

    refactor: deleted some comments

--- src/app/announcements-list/announcement-card/announcement-card.component.ts
+++ src/app/announcements-list/announcement-card/announcement-card.component.ts
@@ -1,10 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ApiService } from '../../services/api-service.service'; // Asegúrate de tener ApiService
-import { AuthService } from '../../services/auth.service'; // Asegúrate de tener AuthService
+import { ApiService } from '../../services/api-service.service'; 
+import { AuthService } from '../../services/auth.service'; 
 import { Announce } from '../../interfaces/announce';
 import Swal from 'sweetalert2';
-import { NgIf } from '@angular/common';
 import { ServiceFactory } from '../../services/api-services/ServiceFactory.service';
 import { CommunityService } from '../../../architecture/services/CommunityService';
 
@@ -12,13 +11,12 @@ import { CommunityService } from '../../../architecture/services/CommunityServic
   selector: 'app-announcement-card',
   templateUrl: './announcement-card.component.html',
   styleUrls: ['./announcement-card.component.css'],
-  imports: [NgIf], // Si usas Angular 14 o superior, puedes usar standalone components
 })
 export class AnnouncementCardComponent implements OnInit {
   
   @Input() announce!: Announce;
   @Input() imgPath!: string;
-  userId!: string;  // Variable para almacenar el userId del usuario autenticado
+  userId!: string;  
 
   constructor(
     private router: Router, 
@@ -28,10 +26,7 @@ export class AnnouncementCardComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    // Obtener el userId del servicio de autenticación
-    this.userId = this.authService.getUserUUID(); // Asegúrate de que este método exista en AuthService
-    console.log('User ID:', this.userId); // Verifica que el userId se obtenga correctamente
-    console.log('Anuncio Publisher ID:', this.announce.publisherID);
+    this.userId = this.authService.getUserUUID(); 
   }
 
   // Método para eliminar el anuncio
@@ -46,7 +41,6 @@ export class AnnouncementCardComponent implements OnInit {
     }).then((result) => {
       if (result.isConfirmed) {
         console.log('ID del anuncio a eliminar:', announcementId);
-        // Hacer la llamada al API de eliminación
         (this.serviceFactory.get('communities') as CommunityService).removeAnnouncement(announcementId).subscribe({
           next: (response) => {
             console.log('Evento eliminado con éxito:', response);
@@ -55,10 +49,8 @@ export class AnnouncementCardComponent implements OnInit {
               text: 'El evento ha sido eliminado correctamente.',
               icon: 'success',              
             }).then(() => {
-              window.location.reload(); // Recargar la página para reflejar los cambios
+              window.location.reload(); 
             })
-            // Aquí puedes agregar lógica para actualizar la vista o redirigir
-            // Por ejemplo, redirigir al usuario a la página de anuncios:
           },
           error: (error) => {
             console.error('Error al eliminar el evento:', error);
