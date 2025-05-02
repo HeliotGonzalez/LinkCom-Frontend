bafb0bf7c9db66154f6d5c84b8d3e54001867ef7
AVR21 <alejandroviera12@gmail.com>
2025-05-02 17:25:39 +0100

    refactor: New directory hierarchy.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,24 @@
 import {Routes} from '@angular/router';
-import {LoginComponent} from './login/login.component';
+import {LoginComponent} from './pages/login/login.component';
 import {HeaderComponent} from './header/header.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {HomepageComponent} from './homepage/homepage.component';
+import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {HomepageComponent} from './pages/homepage/homepage.component';
 import {NoAuthGuard} from './guards/no-auth.guard';
 import {
     FirstStepCommunityCreationComponent
-} from './community-creation/first-step-community-creation/first-step-community-creation.component';
+} from './pages/community-creation/first-step-community-creation/first-step-community-creation.component';
 import {
     SecondStepCommunityCreationComponent
-} from './community-creation/second-step-community-creation/second-step-community-creation.component';
+} from './pages/community-creation/second-step-community-creation/second-step-community-creation.component';
 import {
     ThirdStepCommunityCreationComponent
-} from './community-creation/third-step-community-creation/third-step-community-creation.component';
+} from './pages/community-creation/third-step-community-creation/third-step-community-creation.component';
 import {
     FirstStepEventCreationComponent
-} from './event-creation/first-step-event-creation/first-step-event-creation.component';
+} from './pages/event-creation/first-step-event-creation/first-step-event-creation.component';
 import {
     SecondStepEventCreationComponent
+<<<<<<< HEAD
 } from "./event-creation/second-step-event-creation/second-step-event-creation.component";
 import {ModeratorsManagementComponent} from "./moderators-management/moderators-management.component";
 import {CommunityViewComponent} from "./community-view/community-view.component";
@@ -46,6 +47,19 @@ import { PersonalProfileComponent } from './personal-profile/personal-profile.co
 =======
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
 >>>>>>> 2426d70 (feat: A user can edit their data)
+=======
+} from "./pages/event-creation/second-step-event-creation/second-step-event-creation.component";
+import {ModeratorsManagementComponent} from "./pages/moderators-management/moderators-management.component";
+import {CommunityViewComponent} from "./pages/community-view/community-view.component";
+import {ComunitiesComponent} from './pages/comunities/comunities.component';
+import {EventsCommunityCalendarComponent} from "./pages/events-community-calendar/events-community-calendar.component";
+import { AnnouncementCreationComponent } from './pages/announcement-creation/announcement-creation.component';
+import { AnnouncementsListComponent } from './pages/announcements-list/announcements-list.component';
+import {CommunityRequestsPanelComponent} from "./components/community-requests-panel/community-requests-panel.component";
+import { RegisterFirstStepComponent } from './pages/user-register/register-first-step/register-first-step.component';
+import { RegisterSecondStepComponent } from './pages/user-register/register-second-step/register-second-step.component';
+import { RegisterThirdStepComponent } from './pages/user-register/register-third-step/register-third-step.component';
+>>>>>>> 45cb51e (refactor: New directory hierarchy.)
 
 export const routes: Routes = [
     {path: '', component: DashboardComponent, canActivate: [NoAuthGuard]},
@@ -69,6 +83,7 @@ export const routes: Routes = [
     {path: 'moderators', component: ModeratorsManagementComponent},
     {path: 'announcementsList', component: AnnouncementsListComponent},
 <<<<<<< HEAD
+<<<<<<< HEAD
     {path: 'communityRequestsPanel', component: CommunityRequestsPanelComponent}
 =======
     {path: 'editCommunity', component: CommunityEditComponent}
@@ -86,4 +101,6 @@ export const routes: Routes = [
     {path: 'profile', component: PersonalProfileComponent, outlet: 'modal'},
     { path: 'edit-profile', component: EditProfileComponent } 
 >>>>>>> 2426d70 (feat: A user can edit their data)
+=======
+>>>>>>> 45cb51e (refactor: New directory hierarchy.)
 ];
---
+++ src/app/pages/community-creation/second-step-community-creation/second-community-form-card/second-community-form/second-community-form.component.ts
@@ -0,0 +1,62 @@
+import {Component, Input} from '@angular/core';
+import {FormStepsComponent} from '../../../../../components/form-steps/form-steps.component';
+import {Router} from '@angular/router';
+import {FormService} from "../../../../../services/form-service/form.service";
+import {FormsModule} from "@angular/forms";
+import Swal from "sweetalert2";
+
+@Component({
+    selector: 'app-second-community-form',
+    imports: [
+        FormStepsComponent,
+        FormsModule
+    ],
+    templateUrl: './second-community-form.component.html',
+    standalone: true,
+    styleUrl: './second-community-form.component.css'
+})
+export class SecondCommunityFormComponent {
+    protected formData: FormService | null = null;
+    protected description: string = "";
+    protected privateCommunity: boolean = false;
+
+    constructor(private router: Router, private formService: FormService) {
+    }
+
+    previousPage() {
+        this.router.navigate(["/firstStepCommunityCreation"]).then(r => {
+        });
+        this.saveFormData();
+    }
+
+    nextPage(event: Event) {
+        event.preventDefault()
+        if (this.description === "") {
+            Swal.fire("Error!", "All required fields must be filled!", "error");
+            return;
+        }
+        this.router.navigate(["/thirdStepCommunityCreation"]).then(r => {
+        });
+        this.saveFormData();
+    }
+
+    ngOnInit() {
+        this.formData = this.formService.get("community") as FormService;
+        this.description = this.formData.getOrDefault("description", "");
+        this.privateCommunity = this.formData.getOrDefault("privacy", false);
+    }
+
+    protected saveFormData() {
+        this.formData!.put("description", this.description);
+        this.formData!.put("privacy", this.privateCommunity);
+        this.formService.update();
+    }
+
+    changeFormStep(route: string) {
+        this.saveFormData();
+        if (this.description === "") {
+            Swal.fire("Error!", "All required fields must be filled!", "error");
+            return;
+        }
+    }
+}
