566204031dc31611b28fd13d2a584b204531fa79
AVR21 <alejandroviera12@gmail.com>
2025-05-02 17:32:32 +0100

    fix: broken imports.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import {Routes} from '@angular/router';
 import {LoginComponent} from './pages/login/login.component';
-import {HeaderComponent} from './header/header.component';
+import {HeaderComponent} from './components/header/header.component';
 import {DashboardComponent} from './pages/dashboard/dashboard.component';
 import {HomepageComponent} from './pages/homepage/homepage.component';
 import {NoAuthGuard} from './guards/no-auth.guard';
