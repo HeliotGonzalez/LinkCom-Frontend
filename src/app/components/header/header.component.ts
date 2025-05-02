4f69f8c105c53ce2789946efc6316b1a895aeab3
AVR21 <alejandroviera12@gmail.com>
2025-05-02 17:30:40 +0100

    refactor: added header to components

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
     selector: 'app-header',
