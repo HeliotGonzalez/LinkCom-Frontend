bbbe21ba254cc99bf3a4cc814193841d76eb067d
Javier-Castilla <javier.castilla101@alu.ulpgc.es>
2025-05-12 19:47:31 +0100

    refactor: criteria architecture modified (added FILTERGROUPS).

--- src/architecture/io/criteria/Filters.ts
+++ src/architecture/io/criteria/Filters.ts
@@ -1,4 +1,5 @@
 import {Filter} from "./Filter";
+import {FilterGroup, FilterGroupLogic} from "./FilterGroup";
 
 export interface FilterInterface {
     field: string;
@@ -6,23 +7,39 @@ export interface FilterInterface {
     value: string;
 }
 
-export class Filters {
-    private readonly filters: Filter[];
+export interface FilterGroupInterface {
+    logic: FilterGroupLogic;
+    filters: (FilterInterface | FilterGroupInterface)[];
+}
 
-    constructor(filters: Filter []) {
+export class Filters {
+    private readonly filters: (Filter | FilterGroup)[];
+
+    constructor(filters: (Filter | FilterGroup)[]) {
         this.filters = filters;
     }
 
-    static fromValues(filters: FilterInterface[]): Filters {
-        return new Filters(filters.map(Filter.fromValues));
+    static fromValues(filters: (FilterInterface | FilterGroupInterface)[]): Filters {
+        console.log(filters)
+       const filtersArray: (Filter | FilterGroup)[] = [];
+       filters.forEach(f => filtersArray.push(this.buildFilterFrom(f)));
+        return new Filters(filtersArray)
     }
 
     public getFilters() {
         return [...this.filters];
     }
 
-    static orFromValues(filters: FilterInterface[])  {
-        return new Filters([Filter.buildOrFrom(Filters.fromValues(filters))]);
+    private static isFilterGroup(f: FilterInterface | FilterGroupInterface) {
+        return 'logic' in f;
+    }
+
+    private static buildFilterFrom(f: FilterInterface | FilterGroupInterface) {
+        if (this.isFilterGroup(f)) {
+            return FilterGroup.fromValues(f)
+        } else {
+            return Filter.fromValues(f);
+        }
     }
 }
 
