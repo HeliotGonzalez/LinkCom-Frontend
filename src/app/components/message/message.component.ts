92f773acc5589e729271b9a1aa258172429dc377
Javier-Castilla <javier.castilla101@alu.ulpgc.es>
2025-05-12 20:55:29 +0100

    fix: fixed messages real time.

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -15,22 +15,22 @@ export class MessageComponent {
     @Input() own!: boolean;
     @Output() removeEmitter = new EventEmitter<string>();
 
-    ngOnInit() {
-        console.log(this.message);
-    }
-
     getClass() {
         return this.own ? 'align-items-end' : 'align-items-start';
     }
 
     deserializeDate(dateString: string) {
         const date = new Date(dateString);
-        return `${date.getHours().toString()}:${date.getMinutes().toString()}`
+        return `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`
     }
 
     protected readonly Date = Date;
 
     removeMessage() {
         this.removeEmitter.emit(this.message.id);
+    }
+
+    deserialize(text: string) {
+        return atob(text);
     }
 }
