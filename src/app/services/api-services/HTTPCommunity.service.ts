3efd044af245ba1cd602ea19ecda4411e0b03983
alexxarmaas <alejandro.armas103@alu.ulpgc.es>
2025-05-13 16:05:24 +0100

    adad

--- src/app/services/api-services/HTTPCommunity.service.ts
+++ src/app/services/api-services/HTTPCommunity.service.ts
@@ -127,13 +127,6 @@ export class HTTPCommunityService implements CommunityService {
     createAnnouncement(announcement: CommunityAnnouncement): Observable<ApiResponse<CommunityAnnouncement>> {
         return this.http.post<ApiResponse<CommunityAnnouncement>>(`${this.url}/communities/${announcement.communityID}/createAnnouncement`, announcement);
     }
-    
-    removeAnnouncement(announcementID: string): Observable<ApiResponse<Announce>> {
-        return this.http.delete<ApiResponse<Announce>>(`${this.url}/communities/${announcementID}/deleteAnnouncement`)
-    }
-    
-    removeAnnouncement(announcementID: string): Observable<ApiResponse<Announce>> {
-        return this.http.delete<ApiResponse<Announce>>(`${this.url}/communities/${announcementID}/deleteAnnouncement`)
-    }
+
     
 }
