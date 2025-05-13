8feb8627f0579ffc2520c2b8f28d0e93aee543d4
Mohamed O. Haroun <mohamedhzfp@gmail.com>
2025-05-13 14:32:46 +0100

    feat: add models for reporting

---
+++ src/architecture/model/ReportType.ts
@@ -0,0 +1,8 @@
+export enum ReportType {
+    HATE = "Slurs, Racist or sexist stereotypes, Dehumanization, Incitement of fear or discrimination, Hateful references, Hateful symbols & logos",
+    ABUSE = "Graphic Content, Gratuitous Gore, Adult Nudity & Sexual Behavior, Violent Sexual Conduct, Bestiality & Necrophilia, Media depicting a deceased individual",
+    SPAM = "Fake engagement, scams, fake accounts, malicious links",
+    SENSITIVE = "Insults, Violent Threats, Wish of Harm, Glorification of Violence, Incitement of Violence, Coded Incitement of Violence",
+    PRIVACY = "Sharing private information, threatening to share/expose private information, sharing non-consensual intimate images, sharing images of me that I don’t want on the platform",
+    OTHER = ""   
+}
